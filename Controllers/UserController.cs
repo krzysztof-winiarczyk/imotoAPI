58b726fc3879d3cbd523c1ab8f0f714dfd33b75c
Krzysztof Winiarczyk <krzysztof.r.winiarczyk@gmail.com>
2021-10-24 15:48:42 +0200

    added User Endpoint

---
+++ Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using imotoAPI.Models;
+using imotoAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imotoAPI.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserReturnDto>> GetAll()
+        {
+            var users = _service.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserReturnDto> GetAll([FromRoute] int id)
+        {
+            var user = _service.GetById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<UserReturnDto> Add([FromBody] UserGetDto dto)
+        {
+            var user = _service.Add(dto);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}/contactInfo")]
+        public ActionResult<UserReturnDto> UpdateContactInfo([FromRoute] int id, [FromBody] UserUpdateDto dto)
+        {
+            var user = _service.UpdateContactInfo(id, dto);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}/password")]
+        public ActionResult ChangePassword([FromRoute] int id, [FromBody] PasswordDto dto)
+        {
+            _service.ChangePassword(id, dto);
+            return Ok();
+        }
+    }
+}
