3826701f84e511faa962bb7c8128eb13bab607f6
Krzysztof Winiarczyk <krzysztof.r.winiarczyk@gmail.com>
2021-12-10 16:14:27 +0100

    fix: wrapped messages returned from exceptions into json

--- Middleware/ErrorHandlingMiddleware.cs
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using imotoAPI.Exceptions;
+using imotoAPI.Models;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
@@ -19,47 +20,47 @@ namespace imotoAPI.Middleware
             catch (NotFoundException notFoundException)
             {
                 context.Response.StatusCode = 404;
-                await context.Response.WriteAsync(notFoundException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(notFoundException.Message));
             }
             catch (ForbidException forbidException)
             {
                 context.Response.StatusCode = 403;
-                await context.Response.WriteAsync(forbidException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(forbidException.Message));
             }
             catch (NotAllowedException notAllowedException)
             {
                 context.Response.StatusCode = 405;
-                await context.Response.WriteAsync(notAllowedException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(notAllowedException.Message));
             }
             catch(ResourceExsistsException resourceExsistsException)
             {
                 context.Response.StatusCode = 400;
-                await context.Response.WriteAsync(resourceExsistsException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(resourceExsistsException.Message));
             }
             catch (IncorrectLoggingException incorrectLoginException)
             {
                 context.Response.StatusCode = 401;
-                await context.Response.WriteAsync(incorrectLoginException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(incorrectLoginException.Message));
             }
             catch (LoginNotUniqueException loginNotUniqueException)
             {
                 context.Response.StatusCode = 400;
-                await context.Response.WriteAsync(loginNotUniqueException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(loginNotUniqueException.Message));
             }
             catch (OldNewPasswordException oldNewPasswordException)
             {
                 context.Response.StatusCode = 400;
-                await context.Response.WriteAsync(oldNewPasswordException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(oldNewPasswordException.Message));
             }
             catch (WatchedUserException watchedUserException)
             {
                 context.Response.StatusCode = 400;
-                await context.Response.WriteAsync(watchedUserException.Message);
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto(watchedUserException.Message));
             }
             catch(Microsoft.EntityFrameworkCore.DbUpdateException dbUpdateException)
             {
                 context.Response.StatusCode = 400;
-                await context.Response.WriteAsync("Bad request");
+                await context.Response.WriteAsJsonAsync(new MessageReturnDto("Bad request"));
             }
         }
     }
---
+++ Models/Dtos/MessageReturnDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imotoAPI.Models
+{
+    public class MessageReturnDto
+    {
+        public string Message { get; set; }
+
+        public MessageReturnDto(string message)
+        {
+            Message = message;
+        }
+
+        public MessageReturnDto()
+        {
+
+        }
+    }
+}
